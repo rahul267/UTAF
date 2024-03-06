b19c1c4dd3c6736315976cc5fe55994e409992de
UX508696 <rahul.kumar22@lseg.com>
2024-03-06 14:25:10 +0530

    Add cross browser support

--- Example.Nunit/ReadMe.md
+++ Example.Nunit/ReadMe.md
@@ -4,15 +4,22 @@ Dependency Injection:
 
 Test Configuration:
 
-Logging :
+**Logging**
  1. To enable logging of test case at test level use inbuilt Trace supported by Nunit
    Add Trace.Listeners.Add(new ConsoleTraceListener()); in one time setop and use it in test. [Nunit Log](https://docs.nunit.org/articles/vs-test-adapter/Trace-and-Debug.html)
 2. Logs are also supported at framework level . They can be called by using IloggerServices.Please refer BalzeHomePage.cs.
 
-Reporting :
+**Reporting**
 1. Change the Report name to "Extent"  and corrsponding report will be
    generate in Report folder .
 2. Allure Report will be generated if [AllureNUnit] is added on test class or base test.
     1.  Requires installation of Allure commandline [Allure Report Installation](https://allurereport.org/docs/gettingstarted-installation/ )
     2.  `allure serve` will open the report , depends on how it was installed.
     3. For example if it is installed by NPM . Use ` npx allure-commandline serve`
+
+**Cross Browser Support**
+1. Change the BrowserName property under WebDriver section in TestSetting.json Options are : Chrome, Edge, Firefox,Safari
+1. 
+**Local Driver Support**
+1. Selenium should download the latest browser version , in case selenium is not
+able to do so , Please mention a path `LocalWebDriverPath` in `TestSetting.json`
--- UTAF.Ui/Driver/FirefoxDriverFactory.cs
+++ UTAF.Ui/Driver/FirefoxDriverFactory.cs
@@ -1,12 +1,59 @@
-﻿using System;
+﻿using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UTAF.Ui.Models;
+using OpenQA.Selenium.Firefox;
 
 namespace UTAF.Ui.Driver
 {
     internal class FirefoxDriverFactory
     {
+        private readonly string _localDriverPath;
+        private readonly FirefoxOptions _options;
+        private readonly WebDriverConfiguration _webDriverConfiguration;
+
+        public FirefoxDriverFactory(WebDriverConfiguration configuration)
+        {
+            _webDriverConfiguration = configuration;
+            _localDriverPath = _webDriverConfiguration.LocalWebDriverPath + @"\geckodriver.exe";
+            _options = BuildFirefoxOptions();
+        }
+
+        private FirefoxOptions BuildFirefoxOptions()
+        {
+            var options = new FirefoxOptions();
+            options.AddArguments(_webDriverConfiguration.OptionArgMax,
+                                 "enable-automation",
+                                 "disable-infobars");
+            return options;
+        }
+
+        public IWebDriver CreateDriver()
+        {
+            if (IsLocalDriverAvailable(_localDriverPath))
+            {
+                _options.BinaryLocation = _localDriverPath;
+                return new FirefoxDriver(_localDriverPath, _options);
+            }
+
+            try
+            {
+                return new FirefoxDriver(_options);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to initiate Remote WebDriver: {ex}");
+            }
+
+            return null;
+        }
+
+        private static bool IsLocalDriverAvailable(string driverPath)
+        {
+            return File.Exists(driverPath);
+        }
     }
 }
