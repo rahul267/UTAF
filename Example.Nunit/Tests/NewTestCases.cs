0b0a5590a731a76809f8f9857c23652d6df187ae
Rahul Kumar <rahul267@users.noreply.github.com>
2024-11-26 09:25:41 +0530

    Create Example.Nunit/Tests/NewTestCases.cs

---
+++ Example.Nunit/Tests/NewTestCases.cs
@@ -0,0 +1,24 @@
+using Example.Nunit.DI;
+using Example.Nunit.Pages;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
+using UTAF.Ui.Driver;
+using UTAF.Ui.Providers;
+
+namespace Example.Nunit.Tests
+{
+    public class NewTestCases:BaseTest
+    {
+        [Test]
+        public void NewTestCase1()
+        {
+            // Add your test code here
+        }
+
+        [Test]
+        public void NewTestCase2()
+        {
+            // Add your test code here
+        }
+    }
+}
